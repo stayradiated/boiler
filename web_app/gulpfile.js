4a072b98cd865683d11bbf65505167fde0084a0c
George Czabania <george@czabania.com>
2014-07-31 14:36:14 +1200

    Add web_app

---
+++ web_app/gulpfile.js
@@ -0,0 +1,63 @@
+var gulp         = require('gulp');
+var source       = require('vinyl-source-stream');
+var connect      = require('gulp-connect');
+var react        = require('gulp-react');
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var reactify     = require('reactify');
+var streamify    = require('gulp-streamify');
+var browserify   = require('browserify');
+var watchify     = require('watchify');
+var uglify       = require('gulp-uglify');
+
+gulp.task('default', ['lib', 'style'], function () {
+  gulp.watch('./style/**/*.scss', ['style']);
+
+  return connect.server({
+    root: ['dist'],
+    port: 8000,
+    livereload: true
+  });
+}); 
+
+gulp.task('lib', function () {
+  var bundler = watchify(browserify({
+    cache: {},
+    packageCache: {},
+    fullPaths: true,
+    extensions: '.jsx'
+  }));
+
+  bundler.add('./lib/main.js');
+  bundler.transform(reactify);
+
+  bundler.on('update', rebundle);
+
+  function rebundle () {
+    console.log('rebundling');
+    return bundler.bundle()
+      .on('error', function (err) {
+        console.log(err);
+        throw err;
+      })
+      .pipe(source('main.js'))
+      .pipe(gulp.dest('./dist/js'))
+      .pipe(connect.reload());
+  }
+
+  return rebundle();
+});
+
+gulp.task('style', function () {
+  return gulp.src('./style/main.scss')
+    .pipe(sass({errLogToConsole: true, outputStyle: 'compressed'}))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('minify', function () {
+  return gulp.src('./dist/js/*')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'));
+});
